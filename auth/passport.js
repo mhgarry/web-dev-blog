de51c1940a45545a79ef70af114d4561c0d1fa3e
Matt <mhgarry92@gmail.com>
2023-05-03 05:57:17 -0400

    User model/passport authentication

---
+++ auth/passport.js
@@ -0,0 +1,45 @@
+/* eslint-disable linebreak-style */
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/User');
+// creates a new passport strategy to authenticate instace of User model
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+}, async (email, password, done) => {
+  // tries to find the user by email, if it's not the correct email returns an error message
+	try {
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return done(null, false, { message: 'Incorrect email' });
+    }
+		// tries to find the user by password using the hashed bcrypt password created in user model
+    const isValid = await user.verifyPassword(password);
+    if (!isValid) {
+      return done(null, false, { message: 'Incorrect password' });
+    }
+		// if finds email and password user is validated
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+}));
+// eslint-disable-next-line max-len
+// gives the user access to protected routes through serializing their model instance for this session
+// finds user by the id automatically created by the user model
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+// when the user leaves takes away session access by deserializing the instance
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findByPk(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = {
+  passport,
+};
---
+++ models/User.js
@@ -0,0 +1,72 @@
+/* eslint-disable max-len */
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { check } = require('express-validator');
+const connect = require('../config/connection');
+
+class User extends Model {
+  // rename checkPass to verifyPassword for clarity
+  async verifyPassword(password) {
+    const isValid = await bcrypt.compare(password, this.password);
+    // returns true if password is valid, false otherwise
+    return isValid;
+  }
+}
+
+User.init(
+  {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        notEmpty: true,
+      },
+    },
+    // fix typo in username property
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isAlphanumeric: true,
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        // modify password validation to allow for special characters
+        len: [8, 24],
+        is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+!=])[A-Za-z\d@#$%^&+!=]+$/i,
+      },
+    },
+  },
+  {
+    sequelize: connect,
+    modelName: 'user',
+    hooks: {
+      async beforeCreate(user) {
+        // hash the user's password before storing it in the database
+        const hashPass = await bcrypt.hash(user.password, 10);
+        user.password = hashPass;
+      },
+    },
+
+  },
+);
+// use express-validator to validate email before creating a new user
+// modify checkEmail function to return a Promise that resolves if the email is valid, or rejects with an error message if it is not
+// eslint-disable-next-line arrow-body-style
+User.beforeValidate((user) => {
+  return check(user.email)
+    .notEmpty()
+    .isEmail()
+    .run(user);
+});
+
+module.exports = User;
