e00607cfc6f4e2d42de3c7e55c74c5a2d1fb9293
Matt <mhgarry92@gmail.com>
2023-05-05 00:15:59 -0400

    made comment model

---
+++ models/Comment.js
@@ -0,0 +1,60 @@
+
+const { Model, DataTypes } = require('sequelize');
+const connect = require('../config/connection');
+const User = require('./User');
+const Post = require('./Post');
+
+class Comments extends Model {}
+
+Comments.init(
+	{
+		id: {
+			type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+		},
+		user_id: {
+			type: DataTypes.INTEGER,
+      allowNull: false,
+			references: {
+				model: 'user',
+				key: 'id'
+      },
+	},
+	post_id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		references: {
+			model: 'posts',
+			key: 'id'
+	},
+},
+comment_body: {
+	type: DataTypes.STRING,
+  allowNull: false,
+	validate: {
+		len: [10, 500],
+		isAlphanumeric: true,
+	}
+},
+  created_at: {
+		type: DataTypes.DATE,
+		defaultValue: DataTypes.NOW,
+		allowNull: false,
+		allowNull: false,
+		validate: {
+      isDate: true,
+    },
+	},
+},{
+    sequelize: connect,
+		timestamps: true,
+		createAt: 'created_at',
+		updatedAt: false,
+		modelName: 'comments',
+    },
+);
+
+
+module.exports = Comments;
